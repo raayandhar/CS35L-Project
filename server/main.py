a23b5518a81d9e5118ccd7d6e20c5ff0321fb5ab
ElliottheCat <xiyuan23@g.ucla.edu>
2024-11-24 13:37:30 -0800

    change all ports to 8000 in profileContent.jsx, main.py, login.jsx, profile.jsx

--- server/main.py
+++ server/main.py
@@ -402,4 +402,4 @@ def get_gallery():
 # ----------------- End of Gallery Endpoints ----------------- #
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8000)
