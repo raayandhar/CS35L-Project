5c2f28d36e3f1e77efc63d4238f0b23f24543ce7
kylej21 <kylecj21@gmail.com>
2024-11-26 17:39:18 -0800

    Fixed ports to be uniform

--- client/src/components/profileContent.jsx
+++ client/src/components/profileContent.jsx
@@ -40,7 +40,7 @@ const ProfileContent = ({ user, images, friends }) => {
             return;
         }
 
-        const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:5000';
+        const backendUrl = 'http://127.0.0.1:8000';
 
         try {
             const response = await fetch(`${backendUrl}/add-friend`, {
